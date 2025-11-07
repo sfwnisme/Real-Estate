e1821291334247e7cd45c4f74a2279c87756b416
Safwan Mohamed <sfwn@Safwans-MacBook-Air.local>
2025-11-07 07:55:32 +0300

    feat: youtube video player component

---
+++ components/custom/youtube-video-player.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+  link: string;
+}
+
+export default function YoutubeVideoPlayer({link}: Props) {
+  return (
+    <div className='w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] rounded-2xl overflow-hidden'>
+      <iframe src={link} allowFullScreen width="100%" className="h-full w-full"/>
+    </div>
+  )
+}
