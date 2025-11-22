3b282ac8fed96eca1e9e9b103e420e17af6e9083
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-11-22 10:50:15 +0300

    feat+refactor: create separated schemas and types, and refactor schema variables names

--- features/properties/schema/image-schema.tsx
+++ features/properties/schema/image-schema.tsx
@@ -1,7 +1,7 @@
 import z from "zod";
 import { kbToBytes } from "@/lib/utils";
 
-export const createImageSchema = z.object({
+export const CreateImageSchema = z.object({
   image: z
     .file()
     .max(kbToBytes(1024 * 2), "maximum size is 500KB")
@@ -12,13 +12,17 @@ export const createImageSchema = z.object({
   isFeatured: z.boolean(),
 });
 
-export const createImagesSchema = z
-  .array(createImageSchema)
+export const ImagesArraySchema = z
+  .array(CreateImageSchema)
   .max(15, "upload maximum 15 images");
 
-export type CreateImageType = z.infer<typeof createImageSchema>;
-export type CreateImagesType = z.infer<typeof createImagesSchema>;
+export const CreateArrayImagesSchema = z.object({
+  images: ImagesArraySchema,
+});
 
-export const updateImagesSchema = createImageSchema.omit({ image: true });
+export type CreateImageType = z.infer<typeof CreateImageSchema>;
+export type CreateArrayImagesType = z.infer<typeof CreateArrayImagesSchema>;
 
-export type UpdateImagesType = z.infer<typeof updateImagesSchema>;
+export const UpdateImagesSchema = CreateImageSchema.omit({ image: true });
+
+export type UpdateImagesType = z.infer<typeof UpdateImagesSchema>;
