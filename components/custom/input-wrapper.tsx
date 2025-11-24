2d7d2198adbea1f4576e08497c3a2e99716b97a7
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-11-24 17:58:30 +0300

    close #7: solve inputs label bug, and define extra props

--- components/custom/input-wrapper.tsx
+++ components/custom/input-wrapper.tsx
@@ -4,31 +4,46 @@ import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 
 type Props = {
-  title: string,
-  description?: string,
-  error?: string,
-  disableError?: boolean,
-  name?: string,
-  className?: string,
-  children: React.ReactNode
+  title?: string;
+  description?: string;
+  error?: string;
+  disableError?: boolean;
+  name?: string;
+  className?: string;
+  children: React.ReactNode;
+  childrenClassName?: string;
 };
 
-export default function InputWrapper (props: Props) {
-  const {title, description, error, disableError, name, className, children} = props
+export default function InputWrapper(props: Props) {
+  const {
+    title,
+    description,
+    error,
+    disableError,
+    className,
+    children,
+    childrenClassName,
+  } = props;
   return (
     <div className={cn("flex flex-col w-full", className)}>
-      <Label className="h-[15px]" htmlFor={name}>{title}</Label>
-      <div className="h-2" />
-      {children}
-      <small className="min-h-4 text-xs mt-1 text-primary/60" hidden={!description}>
+      <Label className="h-[15px] flex flex-col items-start gap-2 size-full">
+        {title}
+        <div className={cn("flex flex-col size-full", childrenClassName)}>
+          {children}
+        </div>
+      </Label>
+      <small
+        className="min-h-4 text-xs mt-1 text-primary/60"
+        hidden={!description}
+      >
         {description}
       </small>
-      <small className="min-h-4 text-xs mt-1 text-destructive" hidden={disableError}>
+      <small
+        className="min-h-4 text-xs mt-1 text-destructive"
+        hidden={disableError}
+      >
         {error}
       </small>
     </div>
   );
 }
-
-
-
