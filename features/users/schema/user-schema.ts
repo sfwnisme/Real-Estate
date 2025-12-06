92678d54fb1b7880d2062b860da3afe03cafe59f
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-06 22:27:21 +0300

    feat: user schema

---
+++ features/users/schema/user-schema.ts
@@ -0,0 +1,24 @@
+import { USER_ROLES } from "@/constants/enums";
+import z from "zod";
+
+export const CreateUserSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+  role: z.enum(USER_ROLES, { message: "Invalid role" }),
+});
+
+export type CreateUserSchemaType = z.infer<typeof CreateUserSchema>;
+
+export const UpdateUserSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+  role: z.enum(USER_ROLES, { message: "Invalid role" }),
+});
+
+export type UpdateUserSchemaType = z.infer<typeof UpdateUserSchema>;
